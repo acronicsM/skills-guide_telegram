0504208b50788b449a4cb156cdf21d61b1f2ec2c
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-01 10:51:09 +0300

    Дописал крон новых вакансий по подписке

--- settings.py
+++ settings.py
@@ -9,6 +9,8 @@ load_dotenv()
 class Settings:
     API_KEY = os.getenv('BOT_API_KEY')
     DATABASE_URL = os.getenv('DATABASE_URL')
+    NEW_VACANCIES_CRON_TRIGGER = os.getenv('NEW_VACANCIES_CRON_TRIGGER', default='interval')
+    NEW_VACANCIES_CRON_PERIOD = int(os.getenv('NEW_VACANCIES_CRON_PERIOD', default=60))
 
     SERVER_PARSE = 'http://127.0.0.1:5000'
     SERVER_GPT = 'http://127.0.0.1:7000'
